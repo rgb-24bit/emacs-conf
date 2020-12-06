0e176438f412d4afcb77449b8e98f97212ef5422
rgb-24bit <rgb-24bit@foxmail.com>
2020-12-06 14:17:05 +0800

    feat: add `view-mode' config

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -84,5 +84,18 @@
 
 (add-hook 'prog-mode-hook 'origami-mode)
 
+;; =============================================================================
+;; view mode
+;; =============================================================================
+
+;; https://stackoverflow.com/questions/1128927/how-to-scroll-line-by-line-in-gnu-emacs/16229080#16229080
+(with-eval-after-load 'view
+  (progn
+    (define-key view-mode-map (kbd "n") 'scroll-up-line)
+    (define-key view-mode-map (kbd "p") 'scroll-down-line)))
+
+;; https://emacs.stackexchange.com/questions/3676/how-to-enter-view-only-mode-when-browsing-emacs-source-code-from-help
+(add-hook 'find-function-after-hook 'view-mode)
+
 (provide 'init-utils)
 ;;; init-utils.el ends here
