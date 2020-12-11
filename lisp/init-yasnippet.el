1a270611d1d453eab3105b601a6326709bf7289f
rgb-24bit <rgb-24bit@foxmail.com>
2020-12-11 15:31:50 +0800

    feat: add `snippets' config

--- lisp/init-yasnippet.el
+++ lisp/init-yasnippet.el
@@ -3,11 +3,15 @@
 ;;; Code:
 
 (use-package yasnippet
-    :commands (yas-global-mode yas-minor-mode yas-activate-extra-mode)
-    :init
-    (progn
-      (setq yas-snippet-dirs
-            '((expand-file-name "snippets" user-emacs-directory)))))
+  :commands (yas-global-mode yas-minor-mode yas-activate-extra-mode)
+  :init
+  (progn
+    (setq yas-snippet-dirs
+          '((expand-file-name "snippets" user-emacs-directory))))
+  :config
+  (progn
+    (add-hook 'prog-mode-hook 'yas-minor-mode)
+    (add-hook 'org-mode-hook 'yas-minor-mode)))
 
 (provide 'init-yasnippet)
 ;;; init-yasnippet.el ends here
