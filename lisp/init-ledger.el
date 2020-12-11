a9347310ad801f2e5c0f0339734c9440e9685972
rgb-24bit <rgb-24bit@foxmail.com>
2020-12-11 11:18:26 +0800

    feat: add ledger report config

--- lisp/init-ledger.el
+++ lisp/init-ledger.el
@@ -8,9 +8,21 @@
   :init
   (progn
     (setq ledger-post-amount-alignment-column 62)
-    (add-hook 'ledger-mode-hook
-              (lambda () (setq-local pcomplete-termination-string "")))))
 
-(provide 'init-ledger)
+    (add-hook 'ledger-mode-hook
+              (lambda () (setq-local pcomplete-termination-string "")))
+
+    ;; ledger-reports config
+    (setq ledger-reports
+          '(("bal" "%(binary) -f %(ledger-file) bal")
+            ("cashflow" "%(binary) -f %(ledger-file) bal ^Income ^Expenses")
+            ("networth" "%(binary) -f %(ledger-file) bal ^Assets ^Liabilities")
+            ("monthly" "ledger -f %(ledger-file) bal --period %(month)")
+            ;; ("weekly" "%(binary) -f %(ledger-file) bal --weekly")
+            ("reg" "ledger -f %(ledger-file) reg --date-format %Y/%m/%d")
+            ("payee" "ledger -f %(ledger-file) reg @%(payee) --date-format %Y/%m/%d")
+            ("account" "ledger -f %(ledger-file) reg %(account) --date-format %Y/%m/%d")))))
+
+  (provide 'init-ledger)
 
 ;;; init-ledger.el ends here
