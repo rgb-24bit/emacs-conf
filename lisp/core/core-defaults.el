62c75492f2180aeffee9f429d379c65086ea1f24
rgb-24bit <rgb-24bit@foxmail.com>
2021-03-22 19:48:36 +0800

    feat: add view-mode config

--- lisp/core/core-defaults.el
+++ lisp/core/core-defaults.el
@@ -8,5 +8,12 @@
 ;; always move to help window
 (setq help-window-select t)
 
+;; https://stackoverflow.com/questions/1128927/how-to-scroll-line-by-line-in-gnu-emacs/16229080#16229080
+(with-eval-after-load 'view
+  (progn
+    (define-key view-mode-map (kbd "n") 'scroll-up-line)
+    (define-key view-mode-map (kbd "p") 'scroll-down-line)))
+(add-hook 'find-function-after-hook 'view-mode)
+
 (provide 'core-defaults)
 ;;; core-defaults.el ends here
