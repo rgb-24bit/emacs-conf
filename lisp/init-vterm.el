17f439d0fa186aad3f0abdd6ec2c5593ad42a28c
rgb-24bit <rgb-24bit@foxmail.com>
2021-03-23 13:17:43 +0800

    feat: add shell-pop config

--- lisp/init-vterm.el
+++ lisp/init-vterm.el
@@ -2,6 +2,8 @@
 
 ;;; Code:
 
+(defvar emacsc-shell-default-width 30)
+
 (use-package vterm
   :defer t
   :commands (vterm vterm-other-window)
@@ -14,6 +16,8 @@
     (define-key vterm-mode-map (kbd "M-p") 'vterm-send-up)
     (define-key vterm-mode-map (kbd "M-y") 'vterm-yank-pop)
     (define-key vterm-mode-map (kbd "M-/") 'vterm-send-tab)
+    (define-key vterm-mode-map (kbd "C-y") 'vterm-yank)
+    (define-key vterm-mode-map (kbd "M-m") nil)
 
     (defun emacsc//disable-hl-line-mode ()
       "Locally disable global-hl-line-mode"
@@ -21,6 +25,27 @@
       (setq-local global-hl-line-mode nil))
 
     (add-hook 'vterm-mode-hook 'emacsc//disable-hl-line-mode)))
+
+(defun emacsc/resize-shell-to-desired-width ()
+  (when (and (string= (buffer-name) shell-pop-last-shell-buffer-name)
+	     (memq shell-pop-window-position '(left right)))
+    (enlarge-window-horizontally (- (/ (* (frame-width) emacsc-shell-default-width)
+				       100)
+				    (window-width)))))
+
+(defun emacsc/shell-pop-vterm (index)
+  "Toggle a popup window with `vterm'.
+Multiple shells can be opened with a numerical prefix argument. Using the universal prefix argument will open the shell in the current buffer instead of a popup buffer."
+  (interactive "P")
+  (require 'shell-pop)
+  (if (equal '(4) index)
+      (vterm)
+    (shell-pop--set-shell-type
+     'shell-pop-shell-type '("vterm" "*vterm*" '(lambda () (vterm))))
+    (shell-pop index)
+    (emacsc/resize-shell-to-desired-width)))
+
+(emacsc-leader-def "'" '(emacsc/shell-pop-vterm :which-key "vterm"))
 
 (provide 'init-vterm)
 ;;; init-vterm.el ends here
