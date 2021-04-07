44081623c5190a797c27cc870861b9253604fe75
rgb-24bit <rgb-24bit@foxmail.com>
2021-04-07 21:06:38 +0800

    feat: enable company-yasnippet

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -7,11 +7,20 @@
   :init
   (progn
     (setq company-idle-delay 0.2
-          company-minimum-prefix-length 2
+          company-minimum-prefix-length 1
           company-require-match nil
           company-dabbrev-ignore-case nil
+          company-show-numbers t
           company-dabbrev-downcase nil))
-  (add-hook 'after-init-hook (global-company-mode)))
+  (add-hook 'after-init-hook (global-company-mode))
+  :config
+  (defun company-mode/backend-with-yas (backend)
+    (if (and (listp backend) (member 'company-yasnippet backend))
+        backend
+      (append (if (consp backend) backend (list backend))
+              '(:with company-yasnippet))))
+  (setq company-backends (mapcar #'company-mode/backend-with-yas company-backends)))
+
 
 (provide 'init-company)
 ;;; init-company.el ends here
