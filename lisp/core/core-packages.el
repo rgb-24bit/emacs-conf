a9843652df5245b45fd4cbf092bc6dc17db780ea
rgb-24bit <rgb-24bit@foxmail.com>
2021-04-18 23:34:29 +0800

    feat: use straght.el fork

--- lisp/core/core-packages.el
+++ lisp/core/core-packages.el
@@ -50,19 +50,10 @@
       straight-build-dir (format "build-%s" emacs-version)
       straight-check-for-modifications nil
       straight-enable-package-integration nil
+      straight-use-package-by-default nil
       straight-vc-git-default-clone-depth 1)
 
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
+(require 'straight-bootstrap)
 
 ;; =============================================================================
 ;; init use-package
