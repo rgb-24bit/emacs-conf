512155d2380028af202336041a928a6062a7cf49
rgb-24bit <rgb-24bit@foxmail.com>
2021-04-18 16:46:47 +0800

    feat: keep global key binding

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -5,6 +5,9 @@
 (use-package rust-mode
   :mode "\\.rs\\'"
   :config
+  ;; keep global `C-c C-n' binding
+  (define-key rust-mode-map (kbd "C-c C-n") nil)
+
   (emacsc-leader-def rust-mode-map
     "m"     '(:ignore t :which-key "rust")
 
