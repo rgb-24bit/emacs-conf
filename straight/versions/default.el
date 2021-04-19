1ad357fbe34be6f6e079c7811da84ed6dbde2520
rgb-24bit <rgb-24bit@foxmail.com>
2021-04-19 10:44:59 +0800

    feat: update ignore rule

---
+++ straight/versions/default.el
@@ -0,0 +1,5 @@
+(("el-get" . "84dd1837f9ac80a329ab0c2de6859777f445f8ff")
+ ("emacsmirror-mirror" . "1c24bceb43e0e475656be908133a8eb6df3bf973")
+ ("gnu-elpa-mirror" . "3e7e5862acc80a6371fae2e1bf087442402f7c54")
+ ("melpa" . "935b812125d455df5b53a0ae0f2674e335b1bf3b"))
+:beta
