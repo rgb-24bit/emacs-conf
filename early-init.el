cc9065248d01a5e62f692e830e614362e0310486
rgb-24bit <rgb-24bit@foxmail.com>
2021-04-29 20:16:49 +0800

    feat: update early-init config

--- early-init.el
+++ early-init.el
@@ -2,6 +2,14 @@
 
 ;;; Code:
 
+;; In Emacs 27+, package initialization occurs before `user-init-file' is
+;; loaded, but after `early-init-file'. straight.el handles package
+;; initialization, so we must prevent Emacs from doing it early!
 (setq package-enable-at-startup nil)
+
+;; Resizing the Emacs frame can be a terribly expensive part of changing the
+;; font. By inhibiting this, we easily halve startup times with fonts that are
+;; larger than the system default.
+(setq frame-inhibit-implied-resize t)
 
 ;;; early-init.el ends here
