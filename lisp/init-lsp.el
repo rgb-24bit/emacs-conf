9a808cd4fb06b5c0f10eff3a2362790f917002b5
rgb-24bit <rgb-24bit@foxmail.com>
2021-05-31 10:10:20 +0800

    feat: support lsp mode for js mode

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -87,7 +87,8 @@
 (dolist (hook (list
                'rust-mode-local-vars-hook
                'go-mode-local-vars-hook
-               'python-mode-local-vars-hook))
+               'python-mode-local-vars-hook
+               'js-mode-local-vars-hook))
   (add-hook hook 'emacsc//setup-lsp))
 
 (provide 'init-lsp)
