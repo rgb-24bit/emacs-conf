3ac0de2d8db9996c255399c381914f64b3e71161
rgb-24bit <rgb-24bit@foxmail.com>
2021-08-20 17:31:29 +0800

    feat: change keybinding

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -251,8 +251,8 @@ otherwise return regexp like \"\\\\_<sym\\\\_>\" for the symbol at point."
   (define-key awesome-pair-mode-map (kbd "M-(") 'awesome-pair-wrap-round)
   (define-key awesome-pair-mode-map (kbd "M-)") 'awesome-pair-unwrap)
 
-  (define-key awesome-pair-mode-map (kbd "M-p") 'awesome-pair-jump-right)
-  (define-key awesome-pair-mode-map (kbd "M-n") 'awesome-pair-jump-left)
+  (define-key awesome-pair-mode-map (kbd "M-n") 'awesome-pair-jump-right)
+  (define-key awesome-pair-mode-map (kbd "M-p") 'awesome-pair-jump-left)
   (define-key awesome-pair-mode-map (kbd "M-:") 'awesome-pair-jump-out-pair-and-newline))
 
 ;; =============================================================================
